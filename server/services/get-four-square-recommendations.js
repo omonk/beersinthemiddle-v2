cd63f0509febeec5766cf6c35bc7da8b1f039c71
omonk <olliemonk0@gmail.com>
2018-10-22 22:39:02 +0100

    update server response to get more details of venues

--- server/services/get-four-square-recommendations.js
+++ server/services/get-four-square-recommendations.js
@@ -15,63 +15,55 @@ const fourSquareCredentials = `client_id=${
 const fourSquareBaseUrl = 'https://api.foursquare.com/v2';
 
 const getVenueExploreParams = (ll, section = 'drinks') =>
-  `?ll=${ll}&${fourSquareCredentials}&v=20170509&section=${section}&sortByDistance=1`;
+  `?ll=${ll}&${fourSquareCredentials}&v=20170509&section=${section}&sortByDistance=1&limit=15`;
 
 const getFourSquareRecommendations = ll => {
   const url = `${fourSquareBaseUrl}/venues/explore${getVenueExploreParams(ll)}`;
-  console.log(url);
-
   return fetch(url, fetchOptions).then(res => res.json());
 };
 
-const formatFourSquareResponse = data => {
-  console.log(JSON.stringify(data, null, 2));
-  const venues = data.response.groups[0];
-
-  return venues.items.map(item => ({
-    title: get(item, 'venue.name', 'N/A'),
-    id: get(item, 'venue.id', 'N/A'),
+const formatFourSquareResponse = venues =>
+  venues.map(item => ({
+    title: get(item, 'name', 'N/A'),
+    id: get(item, 'id', 'N/A'),
     location: {
-      address: get(item, 'venue.location.formattedAddress', ['N/A']),
-      lat: get(item, 'venue.location.lat', 'N/A'),
-      lng: get(item, 'venue.location.lng', 'N/A'),
+      address: get(item, 'location.formattedAddress', ['N/A']),
+      lat: get(item, 'location.lat', 'N/A'),
+      lng: get(item, 'location.lng', 'N/A'),
     },
-    url: get(item, 'venue.url', 'N/A'),
+    url: get(item, 'url', 'N/A'),
     price: {
-      priceRange: get(item, 'venue.price.tier', 'N/A'),
+      priceRange: get(item, 'price.tier', 'N/A'),
     },
-    rating: get(item, 'venue.rating', 'N/A'),
-    categories: get(item, 'venue.categories', []).map(
-      category => category.shortName
-    ),
+    rating: get(item, 'rating', 'N/A'),
+    categories: get(item, 'categories', []).map(category => category.shortName),
     hours: {
-      status: get(item, 'venue.hours.status', 'N/A'),
-      isOpen: get(item, 'venue.hours.isOpen', 'N/A'),
+      status: get(item, 'hours.status', 'N/A'),
+      isOpen: get(item, 'hours.isOpen', 'N/A'),
     },
   }));
-};
 
 const getVenueDetails = ({ response }) => {
-  // console.log(response.groups[0].items.map());
   const url = id =>
-    `${fourSquareBaseUrl}/venues/${id}?${fourSquareCredentials}`;
+    `${fourSquareBaseUrl}/venues/${id}?${fourSquareCredentials}&v=20170509`;
 
-  return response.groups[0].items.map(item => {
-    console.log(url(item.id));
-    return fetch(url(item.id), fetchOptions);
-  });
+  const ids = response.groups[0].items.map(item => item.venue.id);
+
+  return ids.map(id =>
+    fetch(url(id), fetchOptions)
+      .then(res => res.json())
+      .then(res => res.response.venue)
+  );
 };
 
 module.exports = (req, res) => {
   const { ll } = req.query;
-  return (
-    getFourSquareRecommendations(ll)
-      .then(res => Promise.all(getVenueDetails(res)))
-      .then(formatFourSquareResponse)
-      .then(response => res.send({ response }))
-      .catch(err => {
-        console.log(`Error: ${err}`);
-        res.status(500).json({ error: err.toString() });
-      })
-  );
+  return getFourSquareRecommendations(ll)
+    .then(res => Promise.all(getVenueDetails(res)))
+    .then(formatFourSquareResponse)
+    .then(response => res.send(response))
+    .catch(err => {
+      console.log(`Error: ${err}`);
+      res.status(500).json({ error: err.toString() });
+    });
 };
