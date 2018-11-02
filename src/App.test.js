649fb563604454d56ab5fd34e5fe21839a40d118
omonk <ollie.monk@thetrainline.com>
2018-11-02 09:21:13 +0000

    update App test - it doesn't work....yet

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import configureStore from 'redux-mock-store';
 
+const mockStore = configureStore();
+const mockStoreInitialState = {
+  recommendations: [],
+  map: {},
+  locations: [],
+  ui: {},
+  geoLocation: {},
+};
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App store={mockStore(mockStoreInitialState)} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
