119ce6511726df5c6bfa48ca5df344478c098d13
Ollie Monk <ollie@olliemonk.com>
2019-09-12 10:55:53 +0800

    start moving to now serverless functions

---
+++ api/foursquare.js
@@ -0,0 +1,66 @@
+const fetch = require('node-fetch');
+const { get } = require('lodash');
+
+const fetchOptions = {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  method: 'get',
+};
+
+const fourSquareCredentials = `client_id=${process.env.FOURSQUARE_API_CLIENT}&client_secret=${process.env.FOURSQUARE_API_SECRET}`;
+
+const fourSquareBaseUrl = 'https://api.foursquare.com/v2';
+
+const getVenueExploreParams = (ll, section = 'drinks') =>
+  `?ll=${ll}&${fourSquareCredentials}&v=20170509&section=${section}&sortByDistance=1&limit=15`;
+
+const getFourSquareRecommendations = ll => {
+  const url = `${fourSquareBaseUrl}/venues/explore${getVenueExploreParams(ll)}`;
+  return fetch(url, fetchOptions).then(res => res.json());
+};
+
+const formatFourSquareResponse = venues =>
+  venues.map(item => ({
+    title: get(item, 'name', 'N/A'),
+    id: get(item, 'id', 'N/A'),
+    location: {
+      address: get(item, 'location.formattedAddress', ['N/A']),
+      lat: get(item, 'location.lat', 'N/A'),
+      lng: get(item, 'location.lng', 'N/A'),
+    },
+    url: get(item, 'url', 'N/A'),
+    price: {
+      priceRange: get(item, 'price.tier', 'N/A'),
+    },
+    rating: get(item, 'rating', 'N/A'),
+    categories: get(item, 'categories', []).map(category => category.shortName),
+    hours: {
+      status: get(item, 'hours.status', 'N/A'),
+      isOpen: get(item, 'hours.isOpen', 'N/A'),
+    },
+  }));
+
+const getVenueDetails = ({ response }) => {
+  const url = id =>
+    `${fourSquareBaseUrl}/venues/${id}?${fourSquareCredentials}&v=20170509`;
+
+  const ids = response.groups[0].items.map(item => item.venue.id);
+
+  return ids.map(id =>
+    fetch(url(id), fetchOptions)
+      .then(res => res.json())
+      .then(res => res.response.venue)
+  );
+};
+
+module.exports = (req, res) => {
+  const { ll } = req.query;
+  return getFourSquareRecommendations(ll)
+    .then(res => Promise.all(getVenueDetails(res)))
+    .then(formatFourSquareResponse)
+    .then(response => res.send(response))
+    .catch(err => {
+      res.status(500).json({ error: err.toString() });
+    });
+};
---
+++ api/reverse-geocode.js
@@ -0,0 +1,28 @@
+const googleMapsClient = require('@google/maps').createClient({
+  key: process.env.GOOGLE_MAPS_API_KEY,
+  Promise: Promise,
+});
+
+module.exports = (req, res) => {
+  const { lat, lng } = req.query;
+
+  if (!lat || !lng) {
+    res.send(new Error('Missing lat or lng'));
+  }
+
+  return googleMapsClient
+    .reverseGeocode({ latlng: { lat, lng } })
+    .asPromise()
+    .then(response => {
+      const body = response.json.results[0];
+      const address = body.formatted_address;
+      const placeId = body.place_id;
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({ address, placeId }));
+    })
+    .catch(err => {
+      if (err === 'timeout') {
+        res.send('timeout');
+      }
+    });
+};
