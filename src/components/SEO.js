a6c74a4e52bf7015996bbf251e47c0dcb395ef06
Ollie Monk <ollie@olliemonk.com>
2019-09-15 00:56:07 +0800

    move meta to index.html

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -11,12 +11,14 @@ const data = {
 
 const SEO = () => (
   <Helmet>
+    <html lang="en" />
     <meta property="og:image" content={socialImage} />
     <meta property="og:type" content="website" />
     <meta property="og:title" content={data.title} />
     <meta property="og:description" content={data.description} />
     <meta property="og:site_name" content={data.title} />
     <meta property="og:url" content={data.siteUrl} />
+    <meta name="twitter:site" content="omonk" />
     <meta name="description" content={data.description} />
   </Helmet>
 );
